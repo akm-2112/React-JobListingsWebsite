2d58d4959a192365c59e95ce268c1479462c2f9f
AungKyawMoe <akyawmoe394@gmail.com>
2025-11-25 21:43:26 +0630

    251125 code

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState} from "react";
 import {FaMapMarker} from "react-icons/fa";
+import {Link} from "react-router-dom";
 
 const JobListing = ({job}) => {
     const [showFUllDescription, setShowFullDescription] = useState(false);
@@ -18,8 +19,9 @@ const JobListing = ({job}) => {
 
                 <div className="mb-5">
                     {description}
+                    <button onClick={()=>setShowFullDescription((prevState)=>!prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{showFUllDescription ? 'Less...' : 'more'}</button>
                 </div>
-                <button onClick={()=>setShowFullDescription((prevState)=>!prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{showFUllDescription ? 'Less' : 'more'}</button>
+
 
                 <h3 className="text-indigo-500 mb-2">{ job.salary } / year</h3>
 
@@ -30,12 +32,12 @@ const JobListing = ({job}) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         { job.location }
                     </div>
-                    <a
-                        href={`/jobs/${job.id}`}
+                    <Link
+                        to={`/jobs/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
